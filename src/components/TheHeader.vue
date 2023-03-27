7794c8673fc50038cdc94289c132a274688621cf
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-27 16:19:40 +0200

    compilated header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,51 @@
 <script>
 export default {
   name: "TheHeader",
+  data() {
+    return {
+        categoriesList: [
+            "CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"
+        ],
+    };
+  },
 };
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <div class="container">
+    <div class="image">
+        <img src="../../src/assets/img/dc-logo.png" alt="">
+    </div>
+
+    <ul class="sectionList">
+        <li v-for="(categorie, index) in categoriesList" :key="index"><a href="#">{{categorie}}</a></li>
+    </ul>
+  </div>
 </template>
 
-<style>
+<style scooped lang="scss">
+
+    .sectionList {
+        list-style: none;
+        flex-grow: 1;
+        display: flex;
+        justify-content: flex-end;
+
+        li {
+            padding: 10px;
+            
+            a {
+                text-decoration: none;
+                color: black;
+
+                &:hover {
+                    color: skyblue;
+                }
+            }
+
+            
+        }
+        
+    }
 
 </style>
