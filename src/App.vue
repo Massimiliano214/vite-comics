8b2545b8130956e55256909b28695453547d9b48
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-27 17:39:49 +0200

    added bonus section

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   import TheHeader from "./components/TheHeader.vue";
   import TheFooter from "./components/TheFooter.vue";
   import BottomBanner from "./components/BottomBanner.vue";
+  import MainBanner from "./components/MainBanner.vue";
 </script>
 
 <template>
   <TheHeader />
+  <main>
+    <div class="container">
+      <h3>  -- &gt; Content goes here &lt;--  </h3>
+    </div>
+  </main>
+  
 
-  <div class="container">
-    <h3>  -- &gt; Content goes here &lt;--  </h3>
-  </div>
-
+  <MainBanner />
   <TheFooter />
   <BottomBanner />
 </template>
@@ -18,5 +22,12 @@
 <style lang="scss">
 
   @use "./styles/general.scss";
+
+  main {
+    background-color: rgb(34, 34, 34);
+    height: 150px;
+    color: white;
+  }
+
 
 </style>
---
+++ src/components/MainBanner.vue
@@ -0,0 +1,52 @@
+<script>
+    export default {
+        name: "MainBanner",
+    };
+</script>
+
+<template>
+    <div class="mainBanner">
+        <div class="containerBannerMain">
+            <ul>
+                <li><img src="../assets/img/buy-comics-digital-comics.png" alt="">DIGITAL COMICS</li>
+                <li><img src="../assets/img/buy-comics-merchandise.png" alt="">DC MERCHANDISE</li>
+                <li><img src="../assets/img/buy-comics-subscriptions.png" alt="">SUBSCRIPTION</li>
+                <li><img src="../assets/img/buy-comics-shop-locator.png" alt="">COMIC SHOP LOCATOR</li>
+                <li><img src="../assets/img/buy-dc-power-visa.svg" alt="">DC POWER VISA</li>
+            </ul>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .mainBanner {
+        background-color: rgb(53, 128, 243);
+
+        .containerBannerMain {
+            widows: 1170px;
+            justify-content: center;
+            display: flex;
+            padding: 30px;
+            
+            ul {
+                display: flex;
+                list-style-type: none;
+                align-items: center;
+
+                li {
+                    display: flex;
+                    align-items: center;
+                    color: white;
+                    padding-left: 20px;
+                    padding-right: 20px;
+                }
+
+                img {
+                    height: 50px;
+                    margin: 10px;
+                }
+            }
+        }
+    }
+</style>
