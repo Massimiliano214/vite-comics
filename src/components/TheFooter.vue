b68cf14c4d33df7d34ab6a288e1f063b51b8904a
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-27 17:47:43 +0200

    creating dinamic lists

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,13 @@
 <script>
 export default {
   name: "TheFooter",
+  data() {
+    return {
+        dcComics: [
+            "CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"
+        ],
+    };
+  },
 };
 </script>
 
@@ -10,19 +17,19 @@ export default {
             <div class="firstList">
                 <h4>DC COMICS</h4>
                 <ul>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
                 </ul>
 
                 <h4>SHOP</h4>
                 <ul>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
                 </ul>
 
             </div>
@@ -30,27 +37,28 @@ export default {
             <div class="firstList">
                 <h4>DC</h4>
                 <ul>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscription</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
                 </ul>
             </div>
            
             <div class="firstList">
                 <h4>SITES</h4>
                 <ul>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
-                    <li><a href="#">lorem</a></li>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC universe</a></li>
+                    <li><a href="#">DC power Visa</a></li>
                 </ul>
             </div>
 
